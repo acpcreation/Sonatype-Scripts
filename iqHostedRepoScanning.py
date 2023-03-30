b3f1fce79c56e5310ff53e08440364ae774993c0
Alexander Plattel <acpcreation@gmail.com>
2023-03-30 09:25:26 -0400

    Update iqHostedRepoScanning.py

--- iqHostedRepoScanning.py
+++ iqHostedRepoScanning.py
@@ -18,21 +18,20 @@ from urllib.parse import urlparse
 iq_app_name_prefix = "Maven-Hosted-NXRM" #Prefix for each app scan
 iq_organization_id = "f9b233f57e994671969baf913f2bf218" #https://help.sonatype.com/iqserver/integrations/nexus-iq-cli#NexusIQCLI-organizationLocatingtheorganizationID
 
-nxrm_url = "http://localhost:8081" #NOT include trailing '/'
+nxrm_url = "http://localhost:8081/" #INCLUDE trailing '/'
 nxrm3_username = "admin"
 nxrm3_password = "admin!23"
 repo_name = "maven-releases"
 
-iq_url = "http://localhost:8070" #NOT include trailing '/'
+iq_url = "http://localhost:8070/" #INCLUDE trailing '/'
 iq_username = "admin"
 iq_password = "admin!23"
 iq_phase = "release"
 path_to_cli_jar = 'nexus-iq-cli-1.156.0-01.jar'
-temp_download_dir = 'nxrm-scan/' #Include trailing '/'
+temp_download_dir = 'nxrm-scan/' #INCLUDE trailing '/'
 
 scanEachIndividualArtifact = True #Scan each individual artifact downloaded
 createMasterReport = True #Create a final scan containing ALL the artifacts in the scan directory
-
 # End of user input
 
 page_number = 0
@@ -40,10 +39,10 @@ def initial_comp_list(page_number):
     page_number = page_number + 1
     print(page_number)
     if page_number == 1:
-        theurl = nxrm_url + "/service/rest/v1/components?repository=" + str(repo_name)
+        theurl = nxrm_url + "service/rest/v1/components?repository=" + str(repo_name)
         print(theurl)
     else:
-        theurl_continuation = nxrm_url + "/service/rest/v1/components?continuationToken=" + str(cont_token) + "&repository=" + str(repo_name)
+        theurl_continuation = nxrm_url + "service/rest/v1/components?continuationToken=" + str(cont_token) + "&repository=" + str(repo_name)
     # fetch report from uri
     res = requests.get(theurl, auth=(nxrm3_username, nxrm3_password))
 
@@ -75,8 +74,8 @@ def initial_comp_list(page_number):
             
             if scanEachIndividualArtifact == True:
                 #Trigger IQ scan
-                print('The IQ scanning CLI command to be invoked is')
-                iq_cli_cmd = "java -jar " + path_to_cli_jar + " -a \'" + iq_username + ":" + iq_password + "\' -i " + iq_app_name_prefix +"-"+ items['name']+ " -s "+ iq_url + " -t "+iq_phase+" -O "+iq_organization_id +" " + temp_download_dir+dlfilename
+                print('The IQ scanning CLI command to be invoked is:')
+                iq_cli_cmd = "java -jar " + path_to_cli_jar + " -a \'" + iq_username + ":" + iq_password + "\' -i " + iq_app_name_prefix +"-"+ dlfilename+ " -s "+ iq_url + " -t "+iq_phase+" -O "+iq_organization_id +" " + temp_download_dir+dlfilename
                 print("\t"+iq_cli_cmd)
                 print()
                 subprocess.call([iq_cli_cmd], shell=True)
@@ -99,7 +98,7 @@ def continued_comp_list(cont_token, page_number):
 
 if __name__ == "__main__":
     cont_token = initial_comp_list(page_number)
-    continued_comp_list(cont_token,page_number)
+    # continued_comp_list(cont_token,page_number)
 
 
 #Based off of Paul Meharg's script: https://github.com/sonatype/se-scripts/blob/e635a3cca7c9ca80ade6029f87cb7c9c00c1d065/paul/NXRM-download-scan-hosted-sbom.py
